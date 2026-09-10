384de5ea4e8cc4e59e0571078de2855b2b0f7721
Ernst Swanepoel <eswanepoel@mecalc.com>
2026-09-10 08:47:22 +0200

    Updated examples to Q2.5.1
    Added new ALO streaming example.
    Added new GPS streaming example.
    Updated ICS421 -> ICS425 and WSB42X2 -> WSB42X6.

---
+++ QClientAdvancedStreamALOData/StreamData.cs
@@ -0,0 +1,191 @@
+﻿// -------------------------------------------------------------------------
+// Copyright (c) Mecalc (Pty) Limited. All rights reserved.
+// -------------------------------------------------------------------------
+//
+// NOTE: The code supplied here are not supported by Mecalc. Use it at own risk!
+// -------------------------------------------------------------------------
+
+using QClient.RestfulClient;
+using QProtocol;
+using QProtocol.Advanced;
+using QProtocol.GenericDefines;
+using QProtocol.InternalModules.ALO;
+using System.Net.Sockets;
+
+namespace AloDataStreaming;
+
+internal class StreamData
+{
+    private readonly string ipAddress;
+    private readonly List<Item> itemList;
+    private readonly RestfulInterface restfulInterface;
+
+    public StreamData(string ipAddress, List<Item> itemList, RestfulInterface restfulInterface)
+    {
+        this.ipAddress = ipAddress;
+        this.itemList = itemList;
+        this.restfulInterface = restfulInterface;
+    }
+
+    public void RunStreamingExample()
+    {
+        // First the Module and Channel Setup
+        var listOfAloModules = new List<ALO42S4Module>();
+        var listOfAloChannels = new List<ALO42S4Channel>();
+        foreach (var item in itemList)
+        {
+            switch (item)
+            {
+                // The Module Setup is basic, set the operation mode to the Arbitrary Waveform option which will configure the channels accordingly.
+                // Then set the sampling rate, please take care that it is set correct  for your expected output frequency of the signal you intend to stream.
+                case ALO42S4Module alo42S4Module:
+                    listOfAloModules.Add(alo42S4Module);
+
+                    alo42S4Module.PutItemOperationMode(ALO42S4Module.OperationMode.ArbitraryWaveform);
+                    var newModuleSettings = alo42S4Module.GetItemSettings<ALO42S4Module.ArbitraryWaveformSettings>();
+                    newModuleSettings.Settings.SampleRate = ALO42S4Module.SampleRate.MsrDivideBy4;
+                    newModuleSettings.Settings.Grounding = ALO42S4Module.Grounding.Floating;
+                    alo42S4Module.PutItemSettings(newModuleSettings);
+                    break;
+
+                // Channel Operation Mode should change to streaming after the Module has been set, for safety you can set it again.
+                // For Streaming, set the TotalSampleCount to 0. You can choose what happens with the signal if the stream gets interrupted, for this example we will fade to 0.
+                case ALO42S4Channel alo42S4Channel:
+                    listOfAloChannels.Add(alo42S4Channel);
+
+                    alo42S4Channel.PutItemOperationMode(ALO42S4Channel.OperationMode.Streaming);
+                    var channelSettings = alo42S4Channel.GetItemSettings<ALO42S4Channel.StreamingSettings>();
+                    channelSettings.Settings.Fallback = ALO42S4Channel.Fallback.Zero;
+                    channelSettings.Settings.OutputVoltageLevel = ALO42S4Channel.OutputVoltageLevel._5V;
+                    alo42S4Channel.PutItemSettings(channelSettings);
+                    break;
+
+                default:
+                    continue;
+            }
+        }
+
+        // Call the apply once after all the settings was set
+        restfulInterface.Put(EndPoints.SystemSettingsApply);
+
+        // QServer will specify a block size which you need to use for the streaming port.
+        var blockSize = (int)listOfAloModules[0].GetBlockSize();
+        if (blockSize == 0)
+        {
+            Console.WriteLine("Could not obtain a valid block size.");
+            return;
+        }
+
+        // Create the data block to be sent to the ALO.
+        var waveData = GenerateSineWave(blockSize * 32);
+        var buffer = new byte[waveData.Length * sizeof(float)];
+        Buffer.BlockCopy(waveData, 0, buffer, 0, buffer.Length);
+
+        while (true)
+        {
+            // Create a task per channel for the data stream, each will independently send data to the DecaQ with a blocking TCP socket.
+            var methodSource = new CancellationTokenSource();
+            var taskSource = new CancellationTokenSource();
+
+            var taskList = new List<Task>();
+            foreach (var channel in listOfAloChannels)
+            {
+                var portNumber = channel.GetPortNumber();
+                var task = new Task(() => SendData(ipAddress, portNumber, buffer, blockSize, methodSource.Token), taskSource.Token, TaskCreationOptions.LongRunning);
+                task.Start();
+                taskList.Add(task);
+            }
+
+            // Allow Buffers to fill and sockets to start blocking
+            Thread.Sleep(1000);
+
+            // Wait for user key press to start output
+            Console.WriteLine("Press any key to enable output...");
+            Console.ReadKey(true);
+
+            // Key pressed, start the output.
+            var startTime = restfulInterface.Put<AloOutputStartTime>(EndPoints.AloStartDataStreaming);
+            Console.WriteLine("Output started...");
+            Console.WriteLine();
+
+            // Wait for user key press to signal cancellation
+            Console.WriteLine("Press any key to stop output, or c to cancel...");
+            var key = Console.ReadKey(true).Key;
+
+            // Stop the ALO output for all Modules. This will also clear the buffers.
+            restfulInterface.Put(EndPoints.AloStopDataStreaming);
+            methodSource.Cancel();
+
+            // Wait for all threads to complete
+            if (Task.WaitAll(taskList.ToArray(), 5000) == false)
+            {
+                taskSource.Cancel();
+            }
+
+            // Check for C, else loop again.
+            if (key == ConsoleKey.C)
+            {
+                break;
+            }
+        }
+
+        // This is not need anymore since the stop endpoint will clear the buffers.
+        // But should you want to ensure the buffers are empty or clear them if incorrect data was sent before, then this is how you'd do it.
+        listOfAloChannels.ForEach(channel => channel.ClearData());
+    }
+
+    /// <summary>
+    /// This method is used to create a sampled sine wave that can be streamed to the ALO Module.
+    /// This is only an example, use it at own risk.
+    /// </summary>
+    /// <param name="dataBlockSize">The data block size of the ALO Channel.</param>
+    /// <returns>An array of sampled sine wave data.</returns>
+    static float[] GenerateSineWave(int dataBlockSize)
+    {
+        var wave = new float[dataBlockSize];
+        var angleStep = 2.0 * Math.PI / dataBlockSize;
+
+        for (var i = 0; i < dataBlockSize; i++)
+        {
+            wave[i] = (float)Math.Sin(angleStep * i);
+        }
+
+        return wave;
+    }
+
+    /// <summary>
+    /// This method will send the analog data to the specified port.
+    /// </summary>
+    /// <param name="serverIp">The IP address of the QServer instance.</param>
+    /// <param name="port">The port of the ALO Channel. This is obtained by calling the GET PortNumber endpoint on the ALO channel.</param>
+    /// <param name="values">An array of floats to be sent to the ALO channel, this must match the given block size.</param>
+    /// <param name="token">A cancellation token to stop the thread.</param>
+    static void SendData(string serverIp, uint port, byte[] byteArray, int blockSize, CancellationToken token)
+    {
+        try
+        {
+            using TcpClient client = new();
+            client.Connect(serverIp, (int)port);
+            Console.WriteLine($"Connected to {serverIp}:{port}");
+
+            using NetworkStream stream = client.GetStream();
+            var index = 0;
+            while (!token.IsCancellationRequested)
+            {
+                stream.Write(byteArray, index, blockSize);
+                index += blockSize;
+                if (index >= byteArray.Length - blockSize)
+                {
+                    index = 0;
+                }   
+            }
+
+            Console.WriteLine($"Disconnected from {serverIp}:{port}");
+        }
+        catch (Exception ex)
+        {
+            // You may want to check token.IsCancellationRequested here to differentiate user cancellation from real errors
+            Console.WriteLine($"Error on port {port}: {ex.Message}");
+        }
+    }
+}
--- QClientAdvancedStreamData/Program.cs
+++ QClientAdvancedStreamData/Program.cs
@@ -14,8 +14,9 @@ using System.Diagnostics;
 using System.Net.Sockets;
 
 Console.WriteLine("QClient Advanced - Stream Data");
-Console.WriteLine("This example will demonstrate how to enable and stream data from Analog, Tacho and CAN FD Channels.");
+Console.WriteLine("This example will demonstrate how to enable and stream data from Analog, Tacho, CAN FD and GPS Channels.");
 Console.WriteLine("Master Sampling Rate and Module setup should be made via QAcquire. Ensure a few channels are enabled.");
+Console.WriteLine("Requirements: DecaQ or MicroQ with at least one Module of any type installed.");
 Console.WriteLine(string.Empty);
 
 // You can either specify the system IP here or in the console when the application runs.
@@ -39,7 +40,8 @@ foreach (var item in itemList)
 {
     // Skip Non Channel types and Unsupported types too.
     if (item.ItemTypeIdentifier != (int)Types.ItemType.Channel
-        || item.ItemNameIdentifier == (int)Types.ChannelType.Unsupported)
+        || item.ItemNameIdentifier == (int)Types.ChannelType.Unsupported
+        || item.ItemNameIdentifier == (int)Types.ChannelType.ALO42S4)
     {
         continue;
     }
@@ -55,6 +57,10 @@ foreach (var item in itemList)
 
     // Then enable the streaming state by fetching the settings.
     var itemSettings = item.GetItemSettings();
+    if (itemSettings.Data == null)
+    {
+        continue;
+    }
 
     // Convert the results from the Generic JSON class to the Data specific class.
     var dataState = Setting.ConvertTo<Data>(itemSettings.Data);
@@ -90,23 +96,12 @@ httpConnection.Put(EndPoints.SystemSettingsApply);
 // QServer can stream to different ports hence query which one is available:
 var streamingSetup = httpConnection.Get<DataStreamSetup>(EndPoints.DataStreamSetup);
 
-Console.WriteLine($"Ready to stream data from http://{ipAddress}:{streamingSetup.TCPPort}. Press C to stop.");
+Console.WriteLine($"Ready to stream data from http://{ipAddress}:{streamingSetup.TCPPort}. Press any key to start and C to stop.");
+Console.ReadKey();
 
 using var tcpClient = new TcpClient(ipAddress, streamingSetup.TCPPort);
 using var networkStreamer = tcpClient.GetStream();
-var buffer = new byte[1024];
-
-// A few variables for pushing updates to the console.
-var timer = Stopwatch.StartNew();
-var lastUpdate = 0l;
-var packetsReceived = 0;
-
-var analogDataPackets = new List<AnalogDataPacket>();
-var canFdDataPackets = new List<CanFdDataPacket>();
-var tachoDataPackets = new List<TachoDataPacket>();
-var analogDataPacketCounter = 0l;
-var canFdDataPacketCounter = 0l;
-var tachoDataPacketCounter = 0l;
+using var binaryReader = new BinaryReader(networkStreamer);
 
 // At this point QServer will start to package data and send it over your port.
 // A Loop for a longer runtime
@@ -115,6 +110,18 @@ Console.WriteLine("Timestamp:");
 Console.WriteLine("Analog Channels:");
 Console.WriteLine("CAN FD Channels:");
 Console.WriteLine("Tacho Channels:");
+Console.WriteLine("GPS Channels:");
+
+var updateTimer = Stopwatch.StartNew();
+var analogPacketCounter = 0;
+var canPacketCounter = 0;
+var tachoPacketCounter = 0;
+var gpsPacketCounter = 0;
+
+var analogDataPackets = new List<AnalogDataPacket>();
+var canFdDataPackets = new List<CanFdDataPacket>();
+var tachoDataPackets = new List<TachoDataPacket>();
+var gpsDataPackets = new List<GpsDataPacket>();
 while (true)
 {
     if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.C)
@@ -122,38 +129,16 @@ while (true)
         break;
     }
 
-    if (networkStreamer.DataAvailable == false)
-    {
-        Thread.Sleep(1);
-        continue;
-    }
-
     // The first step is to read the packet header, it consists of 32 bytes.
-    networkStreamer.ReadExactly(buffer, 0, (int)PacketHeader.BinarySize);
-
-
     // QProtocol has classes defined for all the Data Headers, packets and types.
-    // You'll need to create a BinaryStreamer from your data buffer and feed it into the defined classes.
-    PacketHeader packetHeader;
-    using (var memoryStream = new MemoryStream(buffer))
-    using (var packetHeaderReader = new BinaryReader(memoryStream))
+    var packetHeader = new PacketHeader(binaryReader);
+
+    // Multiple payload types exists, hence you'll need to take care that you can support them all or discard the data when not.
+    // With this example only Payload type 0 will be shown, hence discard the data of others.
+    if (packetHeader.PayloadType != 0)
     {
-        packetHeader = new PacketHeader(packetHeaderReader);
-        packetsReceived++;
-
-        // Increase the buffer size if the payload is too big.
-        if (buffer.Length < packetHeader.PayloadSize)
-        {
-            buffer = new byte[packetHeader.PayloadSize];
-        }
-
-        // Multiple payload types exists, hence you'll need to take care that you can support them all or discard the data when not.
-        // With this example only Payload type 0 will be shown, hence discard the data of others.
-        if (packetHeader.PayloadType != 0)
-        {
-            networkStreamer.ReadExactly(buffer, 0, (int)packetHeader.PayloadSize);
-            continue;
-        }
+        binaryReader.ReadBytes((int)packetHeader.PayloadSize);
+        continue;
     }
 
     // Following the Packet Header is the Payload.
@@ -166,100 +151,97 @@ while (true)
         continue;
     }
 
-    networkStreamer.ReadExactly(buffer, 0, (int)bytesLeft);
-    using (var memoryStream = new MemoryStream(buffer))
-    using (var payloadReader = new BinaryReader(memoryStream))
+    while (bytesLeft > 0)
     {
-        while (bytesLeft > 0)
+        if (bytesLeft < GenericChannelHeader.BinarySize)
         {
-            if (bytesLeft < GenericChannelHeader.BinarySize)
-            {
-                throw new InvalidOperationException($"Invalid payload size. This will only happen when an invalid amount of data was copied from the streamer.");
-            }
+            throw new InvalidOperationException($"Invalid payload size. This will only happen when an invalid amount of data was copied from the streamer.");
+        }
 
-            // First read the Generic Header.
-            var genericChannelHeader = new GenericChannelHeader(payloadReader);
-            bytesLeft -= genericChannelHeader.GetBinarySize();
+        // First read the Generic Header.
+        var genericChannelHeader = new GenericChannelHeader(binaryReader);
+        bytesLeft -= genericChannelHeader.GetBinarySize();
 
-            // Then based on the channel type, a Channel Specific Header might follow.
-            switch (genericChannelHeader.ChannelType)
-            {
-                // For all Analog Channels.
-                case ChannelTypes.Analog:
-                    // Analog Channels will always have a Specific Header.
-                    // It contains information like Data Integrity, minimum and maximum values.
-                    var analogChannelHeader = new AnalogChannelHeader(genericChannelHeader, payloadReader);
-                    bytesLeft -= analogChannelHeader.GetBinarySize();
+        // Then based on the channel type, a Channel Specific Header might follow.
+        switch (genericChannelHeader.ChannelType)
+        {
+            // For all Analog Channels.
+            case ChannelTypes.Analog:
+                // Analog Channels will always have a Specific Header.
+                // It contains information like Data Integrity, minimum and maximum values.
+                var analogChannelHeader = new AnalogChannelHeader(genericChannelHeader, binaryReader);
+                bytesLeft -= analogChannelHeader.GetBinarySize();
 
-                    // And last, read the Data Samples.
-                    // QProtocol provides classes which combines the Headers and Data into one.
-                    var analogDataPacket = new AnalogDataPacket(genericChannelHeader, analogChannelHeader, payloadReader);
-                    bytesLeft -= analogDataPacket.GetBinarySize();
+                // And last, read the Data Samples.
+                // QProtocol provides classes which combines the Headers and Data into one.
+                var analogDataPacket = new AnalogDataPacket(genericChannelHeader, analogChannelHeader, binaryReader);
+                bytesLeft -= analogDataPacket.GetBinarySize();
 
-                    // Store the data in a buffer or save it locally. Just be careful not to stall the thread too much at this point.
-                    analogDataPackets.Add(analogDataPacket);
-                    break;
+                // Store the data in a buffer or save it locally. Just be careful not to stall the thread too much at this point.
+                analogDataPackets.Add(analogDataPacket);
+                break;
 
-                // As with Analog Data, CAN FD has a Specific Header followed by the Data.
-                case ChannelTypes.CanFd:
-                    var canFdChannelHeader = new CanFdChannelHeader(payloadReader);
-                    bytesLeft -= canFdChannelHeader.GetBinarySize();
+            // As with Analog Data, CAN FD has a Specific Header followed by the Data.
+            case ChannelTypes.CanFd:
+                var canFdChannelHeader = new CanFdChannelHeader(binaryReader);
+                bytesLeft -= canFdChannelHeader.GetBinarySize();
 
-                    var canFdDataPacket = new CanFdDataPacket(genericChannelHeader, canFdChannelHeader, payloadReader);
-                    bytesLeft -= canFdDataPacket.GetBinarySize();
+                var canFdDataPacket = new CanFdDataPacket(genericChannelHeader, canFdChannelHeader, binaryReader);
+                bytesLeft -= canFdDataPacket.GetBinarySize();
 
-                    canFdDataPackets.Add(canFdDataPacket);
-                    break;
+                canFdDataPackets.Add(canFdDataPacket);
+                break;
 
-                // Tacho channels does not have a Specific Channel Header, hence we can copy the Packet directly.
-                case ChannelTypes.Tacho:
-                    var tachoDataPacket = new TachoDataPacket(genericChannelHeader, payloadReader);
-                    bytesLeft -= tachoDataPacket.GetBinarySize();
+            // Tacho channels does not have a Specific Channel Header, hence we can copy the Packet directly.
+            case ChannelTypes.Tacho:
+                var tachoDataPacket = new TachoDataPacket(genericChannelHeader, binaryReader);
+                bytesLeft -= tachoDataPacket.GetBinarySize();
 
-                    tachoDataPackets.Add(tachoDataPacket);
-                    break;
+                tachoDataPackets.Add(tachoDataPacket);
+                break;
 
-                // Triggered channels will not be shown in this example.
-                case ChannelTypes.TriggeredData:
-                case ChannelTypes.TriggeredScope:
-                default:
-                    throw new InvalidOperationException($"The channel type received in the stream is not supported by this example.");
-            }
+            // Disclaimer: GPS channels are still in Beta, use it at own risk.
+            case ChannelTypes.Gps:
+                var gpsChannelHeader = new GpsChannelHeader(binaryReader);
+                bytesLeft -= gpsChannelHeader.GetBinarySize();
+                var gpsChannelPacket = new GpsDataPacket(genericChannelHeader, gpsChannelHeader, binaryReader);
+                bytesLeft -= gpsChannelPacket.GetBinarySize();
+
+                gpsDataPackets.Add(gpsChannelPacket);
+                break;
+
+            default:
+                throw new InvalidOperationException($"The channel type received in the stream is not supported by this example.");
         }
     }
 
-    analogDataPacketCounter += analogDataPackets.Count;
-    canFdDataPacketCounter += canFdDataPackets.Count;
-    tachoDataPacketCounter += tachoDataPackets.Count;
-
-    // Now that the entire payload has been read, print the data to the screen and start all over.
-    if (timer.ElapsedMilliseconds > lastUpdate + 250)
+    // Here I'll show the counters that you can visualize how the data is flowing.
+    if (updateTimer.ElapsedMilliseconds > 1000)
     {
-        lastUpdate = timer.ElapsedMilliseconds;
-        var timeStampDateTime = DateTime.MinValue;
+        analogPacketCounter += analogDataPackets.Count;
+        canPacketCounter += canFdDataPackets.Count;
+        tachoPacketCounter += tachoDataPackets.Count;
+        gpsPacketCounter += gpsDataPackets.Count;
 
-        if (analogDataPackets.Count > 0)
-        {
-            var timeStampEpoch = analogDataPackets.First().GenericChannelHeader.Timestamp;
-            timeStampDateTime = DateTimeOffset.FromUnixTimeMilliseconds((long)(timeStampEpoch / 1000000))
-                                              .DateTime;
-        }
+        Console.SetCursorPosition(0, Console.CursorTop - 5);
+        Console.WriteLine($"Timestamp: {packetHeader.TransmitTimestamp:f3} s:");
+        Console.WriteLine($"Analog Channels: {analogPacketCounter} packets received");
+        Console.WriteLine($"CAN FD Channels: {canPacketCounter} packets received");
+        Console.WriteLine($"Tacho Channels: {tachoPacketCounter} packets received");
+        Console.WriteLine($"GPS Channels: {gpsPacketCounter} packets received");
 
-        Console.SetCursorPosition(0, Console.CursorTop - 4);
-        Console.WriteLine($"Runtime: {timer.Elapsed:hh':'mm':'ss} - Packets received: {packetsReceived} - Timestamp: {timeStampDateTime}");
-        Console.WriteLine($"Analog Channel Payloads: {analogDataPacketCounter}");
-        Console.WriteLine($"CAN FD Channel Payloads: {canFdDataPacketCounter}");
-        Console.WriteLine($"Tacho Channel Payloads: {tachoDataPacketCounter}");
+        analogDataPackets.Clear();
+        canFdDataPackets.Clear();
+        tachoDataPackets.Clear();
+        gpsDataPackets.Clear();
+        updateTimer.Restart();
     }
-
-    analogDataPackets.Clear();
-    canFdDataPackets.Clear();
-    tachoDataPackets.Clear();
 }
 
 // Once you are done reading the data from the port remember to close it.
 // This will instruct QServer to stop buffering data an flush the remaining data from the buffers.
 // Hence be careful not to expect a continuous series of samples when opening and closing ports!
+binaryReader.Close();
 networkStreamer.Close();
 tcpClient.Close();
 
